cf64da9ffd13cc793d1be9ef2c4847000bc993cc
Null-logs <null.logs.02@gmail.com>
2022-06-30 17:00:53 -0500

    Funciona cajero e imprime vaquita

--- CajeroAutomatico/ClaseBillete.js
+++ CajeroAutomatico/ClaseBillete.js
@@ -1,1 +1,19 @@
+class Billete{
+    
+    constructor(valor, cantidad){
 
+        //this.imagenBillete = new Image();
+        this.valor = valor;
+        this.cantidad = cantidad;
+
+        //this.imagenBillete.src = imagenesBilletes[this.valor];
+    }
+
+    // mostrarBilletes(){
+
+    //     for (let index = 0; index < this.cantidad; index++) {
+    //         document.body.appendChild(this.imagenBillete);  
+    //     }
+            
+    // }
+}
--- CajeroAutomatico/index.js
+++ CajeroAutomatico/index.js
@@ -1,10 +1,29 @@
-class Billete{
-    
-    constructor(valor, cantidad){
-        this.valor = valor;
-        this.cantidad = cantidad;
+
+function dibujar(){
+
+    if(imagenesBilletes["50"].cargaOk){
+        papel.drawImage(imagenesBilletes["50"].imagen, 0, 0);
     }
 
+    // if(pollo.cargaOk){
+    //     dibujarCuadras(pollo);
+    // }
+
+    // if(cerdo.cargaOk && cerdo.aux == 1){
+    //     papel.drawImage(cerdo.imagen, x, y);
+    // }
+}
+
+function manufacturaDeJsons(imageName){
+    var imageName = {
+        url: "./PakimanesPng/" + imageName + ".png",
+        cargaOk: false
+    }
+    
+    imageName.imagen = new Image();
+    imageName.imagen.src = imageName.url;
+
+    return imageName;
 }
 
 function overlayHTML(peticionDinero){
@@ -18,7 +37,8 @@ function overlayHTML(peticionDinero){
 
         for(var object of billetesEntregados){
             if(object.cantidad > 0){
-                outpuATM.innerHTML = outpuATM.innerHTML + object.cantidad + " Biiletes entregados de $" + object.valor + "<br />";
+                outpuATM.innerHTML = outpuATM.innerHTML + object.cantidad + " Billetes entregados de $" + object.valor + "<br />";
+                //object.mostrarBilletes();
             }
         }
     }
@@ -64,6 +84,18 @@ var billetesEntregados = [];
 var contadorBilletes = 0;
 var NoBilletesEntrega = 0;
 
+var vp = document.getElementById("villaPLatzi");
+var papel = vp.getContext("2d");
+
 var btnExtraerDInero = document.getElementById("btnExtraerDInero");
     btnExtraerDInero.addEventListener("click",requestATM);
 
+var imagenesBilletes = [];
+imagenesBilletes[ "50" ] = manufacturaDeJsons("50");
+imagenesBilletes[ "10" ] = manufacturaDeJsons("10");
+imagenesBilletes[ "20" ] = manufacturaDeJsons("20");
+
+imagenesBilletes["50"].imagen.addEventListener("load", function(){
+    imagenesBilletes["50"].cargaOk = true;
+    dibujar();
+});
